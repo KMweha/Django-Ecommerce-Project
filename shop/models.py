e0a711a8a91418098cd5c4e72556e74653896eeb
Filoha Mariao <mariaolotira@gmail.com.com>
2024-11-23 09:14:47 +0300

    question one

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Customer(models.Model):
     name = models.CharField(max_length=255)  # Customer's full name
-    email = models.EmailField(unique=True)    # Unique email for each customer
+    email = models.EmailField(unique=True)  # Unique email for each customer
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
